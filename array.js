d08e440c21c387844dc121903ef1cb448f04eca1
Luis Enrique <127609212+luiseguzmanvm123@users.noreply.github.com>
2023-03-11 08:36:51 -0600

    Add files via upload

---
+++ array.js
@@ -0,0 +1,39 @@
+/* let cuenta=[5,7,3,4];
+document.write(cuenta[0]);
+for(let i of cuenta){
+    document.write(i)
+} */
+/* let compras=['casa','mesa'];
+console.log(compras);
+compras[0]='tahini';
+console.log(compras); 
+let palabra1 =prompt("primera palabra");
+let palabra2 =prompt("segunda palabra");
+let palabra3 =prompt("tercera palabra");
+let palabras=['hola','bienvenido','palabra','casa','mesa','perro','gato','hogar','lapiz','cuaderno']
+
+
+palabras[2]=palabra1;
+palabras[5]=palabra2;
+palabras[7]=palabra3;
+document.write(palabras);
+/*  */
+
+/* let arreglo=['hola','bienvenido','palabra','casa','mesa'];
+let posicion1=prompt("dime la posicion 1");
+let posicion2=prompt("dime la posicion 2");
+let posicion3=prompt("dime la posicion 3");
+let posicion4=prompt("dime la posicion 4");
+let posicion5=prompt("dime la posicion 5");
+arreglo[0]=posicion1;
+arreglo[1]=posicion2;
+arreglo[2]=posicion3;
+arreglo[3]=posicion4;
+arreglo[4]=posicion5;
+document.write(arreglo); */
+
+
+let array=['dato','dato1']
+array.push('dato3')
+document.write(array);
+
